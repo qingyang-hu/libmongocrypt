08a081ea1b9742c12f4c8e06c5dcd2f782b2c42a
Kevin Albertson <kevin.albertson@mongodb.com>
2019-12-20 13:28:28 -0500

    more

--- .evergreen/linker-tests.sh
+++ .evergreen/linker-tests.sh
@@ -3,6 +3,43 @@
 set -o xtrace
 # set -o errexit
 
+# Directory layout
+# .evergreen
+# -linker_tests_deps
+# --app
+# --bson_patches
+#
+# linker_tests (created by this script)
+# -libmongocrypt-cmake-build (for artifacts build from libmongocrypt source)
+# -app-cmake-build
+# -mongo-c-driver
+# --cmake-build
+# -install
+# --bson1
+# --bson2
+# --libmongocrypt
+#   
+
+if [ ! -e ./.evergreen ]; then
+    echo "Error: run from libmongocrypt root"
+    exit 1;
+fi
+
+libmongocrypt_root=$(pwd)
+linker_tests_root=$(pwd)/linker_tests
+linker_tests_deps_root=$(pwd)/.evergreen/linker_tests_deps
+
+rm -rf linker_tests
+mkdir linker_tests
+mkdir linker_tests/install
+mkdir linker_tests/libmongocrypt-cmake-build
+mkdir linker_tests/app-cmake-build
+cd linker_tests
+
+# Make libbson1 and libbson2
+git clone git@github.com:mongodb/mongo-c-driver.git --depth=1 --config core.eol=lf --config core.autocrlf=false
+cd mongo-c-driver
+
 # Use C driver helper script to find cmake binary, stored in $CMAKE.
 if [ "$OS" == "Windows_NT" ]; then
     CMAKE=/cygdrive/c/cmake/bin/cmake
@@ -15,47 +52,36 @@ fi
 # TEMP
 CMAKE=cmake
 
-# Directory layout
-# linker_tests
-# -libmongocrypt-cmake-build (for artifacts build from libmongocrypt source)
-# -mongo-c-driver
-# --cmake-build
-# -install
-# --bson1
-# --bson2
-# --libmongocrypt
-#   
-
-if [ ! -e ./.evergreen ];
-    echo "Error: run from libmongocrypt root"
-    exit 1;
-fi
-
-libmongocrypt_root=$(pwd)
-linker_tests_root=$(pwd)/linker_tests
-
-rm -rf linker_tests
-mkdir linker_tests
-mkdir linker_tests/install
-mkdir linker_tests/libmongocrypt-cmake-build
-cd linker_tests
-
-# Make libbson1 and libbson2
-git clone git@github.com:mongodb/mongo-c-driver.git --depth=1 --config core.eol=lf --config core.autocrlf=false
-cd mongo-c-driver
-git apply $libmongocrypt_root/.evergreen/bson_patches/libbson1.patch
+git apply $linker_tests_deps_root/bson_patches/libbson1.patch
 mkdir cmake-build
 cd cmake-build
-$CMAKE -DENABLE_MONGOC=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo $ADDITIONAL_CMAKE_FLAGS -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX=$linker_tests_root/install/bson1 ../
+$CMAKE -DBUILD_VERSION=1.16.0-pre -DENABLE_MONGOC=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo $ADDITIONAL_CMAKE_FLAGS -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX=$linker_tests_root/install/bson1 ../
 $CMAKE --build . --target install --config RelWithDebInfo
 # Make libbson2
 cd ..
 git reset --hard
-git apply $libmongocrypt_root/.evergreen/bson_patches/libbson2.patch
+git apply $linker_tests_deps_root/bson_patches/libbson2.patch
 cd cmake-build
-$CMAKE -DENABLE_MONGOC=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo $ADDITIONAL_CMAKE_FLAGS -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX=$linker_tests_root/install/bson2 ../
+$CMAKE -DBUILD_VERSION=1.16.0-pre -DENABLE_MONGOC=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo $ADDITIONAL_CMAKE_FLAGS -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX=$linker_tests_root/install/bson2 ../
 $CMAKE --build . --target install --config RelWithDebInfo
 
 # Build libmongocrypt, static linking against libbson2
-$CMAKE -DCMAKE_BUILD_TYPE=RelWithDebInfo $ADDITIONAL_CMAKE_FLAGS -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_PREFIX_PATH="$linker_tests_root/install/bson2" "-DCMAKE_INSTALL_PREFIX=$linker_tests_root/install/libmongocrypt"  ../
+cd $linker_tests_root/libmongocrypt-cmake-build
+$CMAKE -DCMAKE_BUILD_TYPE=RelWithDebInfo $ADDITIONAL_CMAKE_FLAGS -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_PREFIX_PATH="$linker_tests_root/install/bson2" -DCMAKE_INSTALL_PREFIX="$linker_tests_root/install/libmongocrypt" $libmongocrypt_root
 $CMAKE --build . --target install --config RelWithDebInfo
+
+echo "Test case: Modelling libmongoc's use"
+# app links against libbson1.so
+# app links against libmongocrypt.so
+cd $linker_tests_root/app-cmake-build
+$CMAKE -DCMAKE_BUILD_TYPE=RelWithDebInfo $ADDITIONAL_CMAKE_FLAGS -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_PREFIX_PATH="$linker_tests_root/install/bson1;$linker_tests_root/install/libmongocrypt" $linker_tests_deps_root/app
+$CMAKE --build . --target app --config RelWithDebInfo
+
+check_output () {
+    if [ "$1" != "$2" ]; then
+        echo "got '$1', expecting '$2'"
+        exit 1;
+    fi
+    echo "ok"
+}
+check_output "$(./app)" ".calling bson_malloc0..from libbson1..calling mongocrypt_binary_new..from libbson2."
