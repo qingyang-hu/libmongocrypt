8d8daa063d108614e958c0281689e12b95161c5d
Kevin Albertson <kevin.albertson@mongodb.com>
2022-03-02 14:52:43 -0500

    add stub API for rewrap
    mongocrypt_ctx_setopt_rewrap_many_datakeys
    mongocrypt_ctx_explicit_rewrap_many_datakeys_init

---
+++ src/mongocrypt-ctx-rewrap.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2022-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mongocrypt.h"
+
+bool
+mongocrypt_ctx_setopt_rewrap_many_datakeys (mongocrypt_ctx_t *ctx,
+                                            mongocrypt_binary_t *opts)
+{
+   /* TODO */
+   return true;
+}
+
+bool
+mongocrypt_ctx_explicit_rewrap_many_datakeys_init (mongocrypt_ctx_t *ctx,
+                                                   mongocrypt_binary_t *filter)
+{
+   /* TODO */
+   return true;
+}
