26ac7e49e128d67e667b3ddf6dde5e33324c63b1
Kevin Albertson <kevin.albertson@mongodb.com>
2022-03-28 11:27:36 -0400

    move fle2aead algos into crypto.py

--- etc/crypto.py
+++ etc/crypto.py
@@ -1,11 +1,63 @@
 from cryptography.hazmat.primitives import hashes, hmac
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 import struct
+
+ENCRYPTION_KEY_LENGTH = 32
+MAC_KEY_LENGTH = 32
+HMAC_SHA256_TAG_LENGTH = 32
+IV_LENGTH = 16
 
 def hmacsha256 (key, input):
     hm = hmac.HMAC(key, hashes.SHA256())
     hm.update (input)
     return hm.finalize()
+
+def fle2aead_encrypt(M, Ke, IV, Km, AD):
+    """
+    Do FLE 2 AEAD encryption.
+    See [AEAD with CTR](https://docs.google.com/document/d/1eCU7R8Kjr-mdyz6eKvhNIDVmhyYQcAaLtTfHeK7a_vE/edit#heading=h.35kjadvlcbty)
+    See [aead_encryption_fle2_test_vectors.sh](https://github.com/mongodb/mongo/blob/ecc66915ac757cbeaa7c40eb443d7ec7bffcb80a/src/mongo/crypto/scripts/aead_encryption_fle2_test_vectors.sh#L15) for how server team is generating this.
+    """
+    assert (len(Ke) == ENCRYPTION_KEY_LENGTH)
+    assert (len(IV) == IV_LENGTH)
+    assert (len(Km) == MAC_KEY_LENGTH)
+
+    # S = AES-CTR.Enc(Ke, IV, M)
+    cipher = Cipher(algorithms.AES(Ke), modes.CTR(IV))
+    encryptor = cipher.encryptor()
+    S = encryptor.update(M) + encryptor.finalize()
+
+    # T = HMAC-SHA256(Km, AD || IV || S)
+    T = hmacsha256 (Km, AD + IV + S)
+
+    # C = IV || S || T
+    C = IV + S + T
+    return C
+
+
+def fle2aead_decrypt(C, Km, AD, Ke):
+    assert (len(Ke) == ENCRYPTION_KEY_LENGTH)
+    assert (len(C) > HMAC_SHA256_TAG_LENGTH + IV_LENGTH)
+    assert (len(Km) == MAC_KEY_LENGTH)
+
+    # Parse C as IV || S || T
+    IV = C[0:IV_LENGTH]
+    S = C[IV_LENGTH:-HMAC_SHA256_TAG_LENGTH]
+    T = C[-HMAC_SHA256_TAG_LENGTH:]
+
+    # Compute T' = HMAC-SHA256(Km, AD || IV || S)
+    Tp = hmacsha256 (Km, AD + IV + S)
+    if Tp != T:
+        raise Exception("decryption error")
+
+    # Else compute and output M = AES-CTR.Dec(Ke, S)
+    cipher = Cipher(algorithms.AES(Ke), modes.CTR(IV))
+    decryptor = cipher.decryptor()
+    M = decryptor.update(S) + decryptor.finalize()
+
+    return M
+
 
 def ServerDataEncryptionLevel1Token (rootKey):
     return hmacsha256 (rootKey, struct.pack("<Q", 3))
--- etc/crypto_test.py
+++ etc/crypto_test.py
@@ -7,5 +7,37 @@ class TestCrypto (unittest.TestCase):
         expect = bytes.fromhex("d915ccc1eb81687fb5fc5b799f48c99fbe17e7a011a46a48901b9ae3d790656b")
         self.assertEqual (crypto.ServerDataEncryptionLevel1Token (rootKey), expect)
 
+    def test_fle2_aead_encrypt(self):
+        AD = bytes.fromhex("99f05406f40d1af74cc737a96c1932fdec90")
+        IV = bytes.fromhex("918ab83c8966995dfb528a0020d9bb10")
+        Ke = bytes.fromhex(
+            "c0b091fd93dfbb2422e53553f971d8127f3731058ba67f32b1549c53fce4120e")
+        Km = bytes.fromhex(
+            "50ecc9c6c1a6277ad951f729b3cc6446e21b4024345088a0edda82231a46ca9a")
+        M = bytes.fromhex("74657374310a")
+        expect_C = bytes.fromhex(
+            "918ab83c8966995dfb528a0020d9bb1070cead40b081ee0cbfe7265dd57a84f6c331421b7fe6a9c8375748b46acbed1ec7a1b998387c")
+        got_C = crypto.fle2aead_encrypt(M=M, Ke=Ke, IV=IV, Km=Km, AD=AD)
+        self.assertEqual(expect_C, got_C)
+
+    def test_fle2_aead_decrypt(self):
+        AD = bytes.fromhex("99f05406f40d1af74cc737a96c1932fdec90")
+        Ke = bytes.fromhex(
+            "c0b091fd93dfbb2422e53553f971d8127f3731058ba67f32b1549c53fce4120e")
+        Km = bytes.fromhex(
+            "50ecc9c6c1a6277ad951f729b3cc6446e21b4024345088a0edda82231a46ca9a")
+        expect_M = bytes.fromhex("74657374310a")
+        C = bytes.fromhex(
+            "918ab83c8966995dfb528a0020d9bb1070cead40b081ee0cbfe7265dd57a84f6c331421b7fe6a9c8375748b46acbed1ec7a1b998387c")
+        got_M = crypto.fle2aead_decrypt(C=C, Km=Km, AD=AD, Ke=Ke)
+        self.assertEqual(expect_M, got_M)
+
+        # Test an incorrect HMAC tag.
+        C = bytes.fromhex(
+            "918ab83c8966995dfb528a0020d9bb1070cead40b081ee0cbfe7265dd57a84f6c331421b7fe6a9c8375748b46acbed1ec7a1b9983800")
+        with self.assertRaises(Exception, msg="decryption error"):
+            crypto.fle2aead_decrypt(C=C, Km=Km, AD=AD, Ke=Ke)
+
+
 if __name__ == "__main__":
     unittest.main()
