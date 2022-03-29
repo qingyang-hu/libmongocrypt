48350b032a57d9691d0868911b89c4a225ae2d8a
Kevin Albertson <kevin.albertson@mongodb.com>
2022-03-29 11:19:08 -0400

    add payload 7

--- etc/payload7.py
+++ etc/payload7.py
@@ -1,3 +1,6 @@
+import crypto
+import struct
+
 def bson_type_to_str (subtype):
     """
     See https://bsonspec.org/spec.html
@@ -45,7 +48,72 @@ def bson_type_to_str (subtype):
     if subtype == 0x7F:
         return "Max key"
 
+class Payload7EncryptedData:
+    """
+    struct {
+        uint8_t[length] cipherText; // UserKeyId + EncryptAEAD(K_KeyId, value)
+        uint64_t counter;
+        uint8_t[64] edc;  // EDCDerivedFromDataTokenAndCounter
+        uint8_t[64] esc;  // ESCDerivedFromDataTokenAndCounter
+        uint8_t[64] ecc;  // ECCDerivedFromDataTokenAndCounter
+    }
+    """
+    def __init__ (self, cipherText : bytes, counter : int, edc : bytes, esc : bytes, ecc : bytes):
+        self.cipherText = cipherText
+        self.counter = counter
+        self.edc = edc
+        self.esc = esc
+        self.ecc = ecc
+    
+    def fromBytes (bytesin):
+        # Q: what are the first 8 bytes? A: The encoding of the length of cipherText.
+        cipherText_len = bytesin[0:8]
+        bytesin = bytesin[8:]
+
+        cipherText_len = len(bytesin) - (8 + (3 * 32))
+        cipherText = bytesin[0:cipherText_len]
+        bytesin = bytesin[cipherText_len:]
+        counter = struct.unpack("<Q", bytesin[0:8])[0]
+
+        bytesin = bytesin[8:]
+        edc = bytesin[0:32]
+        bytesin = bytesin[32:]
+        esc = bytesin[0:32]
+        bytesin = bytesin[32:]
+        ecc = bytesin[0:32]
+        bytesin = bytesin[32:]
+        # Check that all was consumed.
+        if len(bytesin) > 0:
+            raise Exception ("Unexpected extra data in Payload7EncryptedData: {}".format(bytesin.hex()))
+        return Payload7EncryptedData (cipherText, counter, edc, esc, ecc)
+
+    def decrypt (self, K_Key):
+        UserKeyId = self.cipherText[0:16]
+        AD = UserKeyId
+        C = self.cipherText[16:]
+        # print (len(C) - 16 - 32, C.hex())
+        dek = crypto.DEK (K_Key)
+        return crypto.fle2aead_decrypt (C, dek.Km, AD, dek.Ke)
+
+    def __str__ (self):
+        ret = "Payload7EncryptedData ... begin\n"
+        ret += " cipherText={}\n".format(self.cipherText)
+        ret += " counter={}\n".format(self.counter)
+        ret += " edc={}\n".format(self.edc)
+        ret += " esc={}\n".format(self.esc)
+        ret += " ecc={}\n".format(self.ecc)
+        ret += "Payload7EncryptedData ... end\n"
+        return ret
+
 class Payload7:
+    """
+    struct {
+        uint8_t fle_blob_subtype = 7;
+        uint8_t key_uuid[16]; // S_KeyId aka IndexKeyId
+        uint8  original_bson_type;
+        ciphertext[ciphertext_length];
+    }
+    """
     def __init__ (self, key_uuid : bytes, original_bson_type : int, ciphertext : bytes):
         self.key_uuid = key_uuid
         self.original_bson_type = original_bson_type
@@ -64,15 +132,18 @@ class Payload7:
         p7 = Payload7 (key_uuid, original_bson_type, ciphertext)
         return p7
     
-    def toPlaintext (self, S_Key, K_Key):
-        # First Decrypt the ciphertext with ServerDataEncryptionLevel1Token.
-        # TODO.
-        
-        return b""
+    def getEncryptedData (self, S_Key):
+        # Decrypt the ciphertext with ServerDataEncryptionLevel1Token.
+        dek = crypto.DEK (S_Key)
+        ServerDataEncryptionLevel1Token = crypto.ServerDataEncryptionLevel1Token (dek.TokenKey)
+        p7ed = crypto.fle2_decrypt (self.ciphertext, ServerDataEncryptionLevel1Token)
+        return Payload7EncryptedData.fromBytes (p7ed)
 
     def __str__ (self):
-        ret = "fle_blob_subtype = 7\n"
-        ret += "key_uuid = {}".format(self.key_uuid.hex()) + "\n"
-        ret += "original_bson_type = {} ({})".format(self.original_bson_type, bson_type_to_str (self.original_bson_type)) + "\n"
-        ret += "ciphertext = {}".format(self.ciphertext.hex()) + "\n"
+        ret = "Payload7 ... begin\n"
+        ret += " fle_blob_subtype = 7\n"
+        ret += " key_uuid = {}".format(self.key_uuid.hex()) + "\n"
+        ret += " original_bson_type = {} ({})".format(self.original_bson_type, bson_type_to_str (self.original_bson_type)) + "\n"
+        ret += " ciphertext = {}".format(self.ciphertext.hex()) + "\n"
+        ret += "Payload7 ... end\n"
         return ret
--- etc/payload7_test.py
+++ etc/payload7_test.py
@@ -2,6 +2,7 @@ import unittest
 from payload7 import Payload7
 import base64
 import re
+import struct
 
 def from_uuid_hex (uuid_hex):
     """
@@ -21,5 +22,32 @@ class TestPayload7 (unittest.TestCase):
         self.assertEqual(p7.key_uuid, from_uuid_hex("12345678-1234-9876-1234-123456789012"))
         self.assertEqual(p7.original_bson_type, 2)
 
+    def test_Payload7_getEncryptedData (self):
+        input = bytes.fromhex("07123456781234987612341234567890120243bba14ddf42da823c33569f4689f465a606d2cea84e6b7468167d90ee12e269b9bc8774d41b16eed047cad03ca432762225810a66a1dce187d8ce044fb3d2a9e9100f8824502a3825e12db71e328f4e4ebb80fac9952661f54a98496381ed7a342c4a9bb22bf60be642ca7cc75c2a181ce99dd03a824a85c6cc5fbd0fdc22a3b0316f5d1934d6b1f2a07be8d890250814c7e6b3e5f20bff1ebd08638c0faa47a784995f8dfe4c2947b43b4c97b4970539930da449edff2a23ca4596536e7f339da76fc9c7c9d1c09619a77d49")
+        p7 = Payload7.fromBytes(input)
+        
+        S_Key = bytes.fromhex("7dbfebc619aa68a659f64b8e23ccd21644ac326cb74a26840c3d2420176c40ae088294d00ad6cae9684237b21b754cf503f085c25cd320bf035c3417416e1e6fe3d9219f79586582112740b2add88e1030d91926ae8afc13ee575cfb8bb965b7")
+        p7ed = p7.getEncryptedData (S_Key)
+        self.assertEqual(p7ed.counter, 1)
+        
+
+    def test_Payload7_getEncryptedData_decrypt (self):
+        input = bytes.fromhex("07123456781234987612341234567890120243bba14ddf42da823c33569f4689f465a606d2cea84e6b7468167d90ee12e269b9bc8774d41b16eed047cad03ca432762225810a66a1dce187d8ce044fb3d2a9e9100f8824502a3825e12db71e328f4e4ebb80fac9952661f54a98496381ed7a342c4a9bb22bf60be642ca7cc75c2a181ce99dd03a824a85c6cc5fbd0fdc22a3b0316f5d1934d6b1f2a07be8d890250814c7e6b3e5f20bff1ebd08638c0faa47a784995f8dfe4c2947b43b4c97b4970539930da449edff2a23ca4596536e7f339da76fc9c7c9d1c09619a77d49")
+        p7 = Payload7.fromBytes(input)
+        
+        S_Key = bytes.fromhex("7dbfebc619aa68a659f64b8e23ccd21644ac326cb74a26840c3d2420176c40ae088294d00ad6cae9684237b21b754cf503f085c25cd320bf035c3417416e1e6fe3d9219f79586582112740b2add88e1030d91926ae8afc13ee575cfb8bb965b7")
+        p7ed = p7.getEncryptedData (S_Key)
+        self.assertEqual(p7ed.counter, 1)
+
+        K_Key = bytes.fromhex ("a7ddbc4c8be00d51f68d9d8e485f351c8edc8d2206b24d8e0e1816d005fbe520e489125047d647b0d8684bfbdbf09c304085ed086aba6c2b2b1677ccc91ced8847a733bf5e5682c84b3ee7969e4a5fe0e0c21e5e3ee190595a55f83147d8de2a")
+        plaintext = p7ed.decrypt (K_Key)
+        # plaintext is everything after e_name in https://bsonspec.org/spec.html.
+        string_len = struct.unpack ("<I", plaintext[0:4])[0]
+        plaintext = plaintext[4:]
+        self.assertEqual (string_len, 9)
+        string = plaintext[0:string_len]
+        plaintext = plaintext[string_len:]
+        self.assertEqual (string, b"value123\x00")
+        self.assertEqual (plaintext, b"")
 if __name__ == "__main__":
     unittest.main()
