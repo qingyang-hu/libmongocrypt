3fb880fd2f4ae01daf455d1d3437ba463409feb5
Kevin Albertson <kevin.albertson@mongodb.com>
2022-05-15 10:29:56 -0400

    use my path to csfle

--- bindings/python/test/test_mongocrypt.py
+++ bindings/python/test/test_mongocrypt.py
@@ -379,9 +379,7 @@ class TestMongoCryptCallback(unittest.TestCase):
 
     def test_csfle(self):
         encrypter = AutoEncrypter(MockCallback(), self.mongo_crypt_opts(),
-            csfle_path=os.path.expanduser("~/csfle/lib/mongo_csfle_v1.dylib",),
-            csfle_required=True,
-            )
+                                  csfle_path="/Users/kevin.albertson/bin/csfle/lib/mongo_csfle_v1.dylib")
         self.addCleanup(encrypter.close)
         ref = lib.mongocrypt_csfle_version(encrypter.mongocrypt.crypt)
         self.assertGreater(int(ffi.cast("intptr_t", ref)), 0)
